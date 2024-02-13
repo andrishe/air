da3b24d7497128eb8af72ff78f36b5f0cb23af95
andrishe <andrishelhyan@gmail.com>
2024-02-13 17:38:33 +0100

    concat

---
+++ 003-concat.js
@@ -0,0 +1,15 @@
+const readlineSync = require("readline-sync");
+
+const splitStr = (str) => {
+  if (!isNaN(str)) {
+    console.log("Error entrez une phrase");
+    process.exit(1);
+  }
+
+  const strSplit = str.split(" ").join(" ");
+  return strSplit;
+};
+
+const str = readlineSync.question("Entrez votre phrase: ");
+
+console.log(splitStr(str));
